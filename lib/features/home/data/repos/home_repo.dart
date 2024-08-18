480e7087bc605faa540805668f2b66d914e9f1bd
Ahmedjk4 <botabasha1@gmail.com>
2024-08-19 02:57:49 +0300

    created failures class and used it in home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,7 +1,8 @@
+import 'package:book_store/core/errors/failures.dart';
 import 'package:book_store/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Exception, List<BookModel>>> fetchFeaturedBooks();
-  Future<Either<Exception, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
 }
