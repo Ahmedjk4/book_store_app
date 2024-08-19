b34b1a447a2e351e222929735a2499a11adb7b07
Ahmedjk4 <botabasha1@gmail.com>
2024-08-20 02:47:38 +0300

    created api_service and implemented fetchNewestBooks method for home repo

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,47 @@
+import 'package:book_store/core/errors/failures.dart';
+import 'package:book_store/core/utils/api_service.dart';
+import 'package:book_store/features/home/data/models/book_model/book_model.dart';
+import 'package:book_store/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        try {
+          books.add(BookModel.fromJson(item));
+        } catch (e) {
+          books.add(BookModel.fromJson(item));
+        }
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
