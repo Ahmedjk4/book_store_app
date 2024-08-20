c940ce5b0bc52dd29e64e72570e066614b3b2099
Ahmedjk4 <botabasha1@gmail.com>
2024-08-20 03:50:46 +0300

    provided cubits

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:book_store/constants.dart';
 import 'package:book_store/core/utils/app_router.dart';
 import 'package:book_store/core/utils/app_theme.dart';
 import 'package:book_store/core/utils/service_locator.dart';
+import 'package:book_store/features/home/data/repos/home_repo_impl.dart';
+import 'package:book_store/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart';
+import 'package:book_store/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,10 +30,24 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: AppTheme.darkTheme,
-      title: 'Bookly Store',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          )
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: AppTheme.darkTheme,
+        title: 'Bookly Store',
+      ),
     );
   }
 }
