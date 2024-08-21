1b182e6b101ee2a08d767389c868bd834e867292
Ahmedjk4 <botabasha1@gmail.com>
2024-08-21 07:19:23 +0300

    display data part 1

--- lib/features/home/presentation/views/widgets/featured_books_carousel.dart
+++ lib/features/home/presentation/views/widgets/featured_books_carousel.dart
@@ -1,8 +1,12 @@
 import 'package:book_store/core/utils/app_router.dart';
 import 'package:book_store/core/utils/assets.dart';
+import 'package:book_store/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import '../../../../../core/widgets/custom_error_widget.dart';
+import '../../../../../core/widgets/custom_loading_indicator.dart';
 import 'custom_book_image.dart';
 
 class FeaturedBooksCarousel extends StatelessWidget {
@@ -12,32 +16,44 @@ class FeaturedBooksCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0),
-      child: CarouselSlider(
-        items: [
-          GestureDetector(
-            onTap: () {
-              context.push(AppRouter.bookDetailsPath);
-            },
-            child: const CustomBookImage(
-              image: AppAssets.testBookImage,
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(
+            errMessage: state.errMessage,
+          );
+        } else if (state is FeaturedBooksSuccess) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: CarouselSlider(
+              items: [
+                GestureDetector(
+                  onTap: () {
+                    context.push(AppRouter.bookDetailsPath);
+                  },
+                  child: const CustomBookImage(
+                    image: AppAssets.testBookImage,
+                  ),
+                ),
+              ],
+              options: CarouselOptions(
+                padEnds: false,
+                height: MediaQuery.sizeOf(context).height * .3,
+                viewportFraction: 0.5,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                enlargeCenterPage: true,
+                enlargeFactor: 0.25,
+                pageSnapping: true,
+                scrollDirection: Axis.horizontal,
+              ),
             ),
-          ),
-        ],
-        options: CarouselOptions(
-          padEnds: false,
-          height: MediaQuery.sizeOf(context).height * .3,
-          viewportFraction: 0.5,
-          initialPage: 0,
-          enableInfiniteScroll: true,
-          reverse: false,
-          enlargeCenterPage: true,
-          enlargeFactor: 0.25,
-          pageSnapping: true,
-          scrollDirection: Axis.horizontal,
-        ),
-      ),
+          );
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
